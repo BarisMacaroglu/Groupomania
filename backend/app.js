9623ee66ddc99119f6e0ca74c0ac2e4f97aab2e8
Baris Macaroglu <barismacaroglu@hotmail.com>
2021-11-02 12:14:35 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config({
+    path: "./config.env"
+});
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+module.exports = app;
