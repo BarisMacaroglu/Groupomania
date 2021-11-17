85d740e5dac735b0e62626d9f0d5d0429ebac1bf
Baris Macaroglu <barismacaroglu@hotmail.com>
2021-11-17 14:22:30 +0100

    input validations applied to necessary routes

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
+const validateInput = require("../middleware/validate-inputs");
 
 const ctrlPost = require("../controllers/post");
 
-router.post('/new', auth, multer, ctrlPost.newPost);
+router.post('/new', auth, multer, validateInput.postContent, ctrlPost.newPost);
 router.get('/', auth, ctrlPost.getAllPosts);
 router.get('/:id', auth, ctrlPost.getOnePost);
 router.delete('/:id', auth, ctrlPost.deletePost);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,19 +2,20 @@ const express = require('express');
 const router = express.Router();
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
+const validateInput = require("../middleware/validate-inputs");
 
 const ctrlUser = require("../controllers/user");
 
-router.post("/signup", ctrlUser.signup);
-router.post("/login", ctrlUser.login);
+router.post("/signup", validateInput.signup, ctrlUser.signup);
+router.post("/login", validateInput.login, ctrlUser.login);
 router.get("/logout", auth, ctrlUser.logout);
 router.get('/currentuser', auth, ctrlUser.getCurrentUser);
 router.get("/users", auth, ctrlUser.getAllUsers);
-router.get("/users/search", auth, ctrlUser.searchUsers);
+router.get("/users/search", auth, validateInput.searchUser, ctrlUser.searchUsers);
 router.get("/users/:id", auth, ctrlUser.getOneUser);
-router.put("/users/:id/description", auth, ctrlUser.changeDescription);
+router.put("/users/:id/description", auth, validateInput.description, ctrlUser.changeDescription);
 router.put('/users/:id/picture', auth, multer, ctrlUser.changeProfilePicture);
-router.put("/users/:id/password", auth, ctrlUser.changePassword);
+router.put("/users/:id/password", auth, validateInput.changePassword, ctrlUser.changePassword);
 router.put("/users/:id/admin", auth, ctrlUser.changeAdmin);
 router.delete("/users/:id", auth, ctrlUser.deleteAccount);
 
